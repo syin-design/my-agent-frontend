43de8dc52542b9f84fcaedae23813235f0c330ef
syin-design <syin@gm.slc.edu>
2026-08-05 03:19:35 +0800

    改用传统JS打包方式，彻底解决国产浏览器兼容问题

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-      renderModernChunks: false
-    })
-  ],
+  plugins: [react()],
+  build: {
+    target: 'es2015',
+    rollupOptions: {
+      output: {
+        format: 'iife',
+        entryFileNames: 'assets/app.js',
+      }
+    }
+  }
 })
