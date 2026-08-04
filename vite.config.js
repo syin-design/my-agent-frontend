6565f8f2634d8a58364c9627f202555d3aabd7c5
syin-design <syin@gm.slc.edu>
2026-08-05 02:55:52 +0800

    添加老旧浏览器兼容支持

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
+plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
 })
